d1bfa923d2c6d33f1af46d948be5be7c91e2da32
Diego Franco <franck312001@gmail.com>
2025-11-06 17:05:27 -0500

    Subida completa de API20252

--- src/app.js
+++ src/app.js
@@ -3,42 +3,42 @@ import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-//importar las rutas
+// importar rutas
 import clientesRoutes from './routes/clientes.routes.js'
 import productosRoutes from './routes/productos.routes.js'
 import usuariosRoutes from './routes/usuarios.routes.js'
 import pedidosRoutes from './routes/pedidos.routes.js'
 
-//definir los modulos de entrada
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+// configuración de __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// configurar CORS
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+};
 
-//definir los permisos
-const corsOptions={
-    origin:'*', //la direccion del dominio del servidor
-    methods:['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials:true
-}
-
-
-const app=express();
+const app = express();
 app.use(cors(corsOptions));
-app.use(express.json()) //interpretar objetos json
-app.use(express.urlencoded({extended:true})) //se añade para poder receptar por unidad
-app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// ✅ servir carpeta uploads con ruta absoluta
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-//indicar que rutas se utilice ojo
-app.use('/api/clientes', clientesRoutes)
-app.use('/api/productos', productosRoutes)
-app.use('/api', usuariosRoutes)
-app.use('/api', pedidosRoutes)
+// rutas API
+app.use('/api/clientes', clientesRoutes);
+app.use('/api/productos', productosRoutes);
+app.use('/api', usuariosRoutes);
+app.use('/api', pedidosRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:' Endponit not fount'
-    })
-})
+// manejo de error 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Endpoint not found'
+  });
+});
 
 export default app;
